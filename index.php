e79a7acfc66b0d3ea248470d5bcd87ec9984d959
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2018-10-12 19:32:44 -0300

    Criando pagina inicial e configurcao do projeto

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php include_once 'view/dependencias.php'; ?>
+	</head>
+	<body>
+		<div class="container">
+			<h2 class="text-center">
+			List of Clients <i class="fa fa-list"></i>
+			</h2>
+			<h5 class="text-right">
+			<a href="view/page_register.php" class="btn btn-primary btn-xs">
+				<i class="fa fa-user-plus"></i>
+			</a>
+			</h5>
+			<div class="table-responsive">
+				<table class="table table-hover">
+					<thead>
+						<tr>
+							<th>ID</th>
+							<th>NOME</th>
+							<th>E-MAIL</th>
+							<th>CPF</th>
+							<th>DT. NASCIMENTO</th>
+							<th>ENDEREÇO</th>
+							<th>TELEFONE</th>
+							<th colspan="3">AÇÕES</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td>
+								<form method="POST">
+									<button class="btn btn-warning  btn-xs">
+									<i class="fa fa-user-edit"></i>
+									</button>
+								</form>
+							</td>
+							<td>
+								<form method="POST" onclick="return confirm('Você tem certeza que deseja excluir?);">
+									<button class="btn btn-danger btn-xs">
+										<i class="fa fa-trash"></i>
+									</button>
+								</form>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</body>
+</html>
